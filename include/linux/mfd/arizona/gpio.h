94baa94636b0318a7cd2b0074ef1f85dd5318684
MandySingh <mandeeps139@gmail.com>
2017-03-19 22:15:13 +0530

    First commit

---
+++ include/linux/mfd/arizona/gpio.h
@@ -0,0 +1,16 @@
+/*
+ * GPIO configuration for Arizona devices
+ *
+ * Copyright 2013 Wolfson Microelectronics. PLC.
+ *
+ * Author: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _ARIZONA_GPIO_H
+#define _ARIZONA_GPIO_H
+
+#endif
